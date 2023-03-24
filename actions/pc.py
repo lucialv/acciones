06d41305333439dccbe479a8ae5daa69f045e5d0
giinneer <128838988+giinneer@users.noreply.github.com>
2023-03-24 19:50:34 +0100

    better pc.py

--- actions/pc.py
+++ actions/pc.py
@@ -4,5 +4,9 @@ def pc():
     tiempo = None
     if frase == frase_random:
         print(f"Has escrito la frase en {tiempo}")
+    elif tiempo == 10: #si han pasado de 10s sale ese mensaje
+        print(f"Podrias mejorar tus dotes con el teclado")
+    elif tiempo == 20:#si han pasado de 20s sale ese mensaje
+        print(f"Te recomiendo un curso de mecanografía")
     else:
         print("No creo que hayas escrito la frase correctamente!")

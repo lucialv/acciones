25e403a32d10286c02787284b592aa431a8290dd
Mishu <124211865+mishhuu@users.noreply.github.com>
2023-03-24 09:21:21 +0000

    Updated readme.md

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Acciones
 
 Python program that do actions.
+
+## Usage
+> To run this program you must need [Python](https://www.python.org/downloads/)
+If you already have Python you only need to open "acciones.py"
+
+## Warning
+> This program will only work on Windows, later Im going to update to use it on Linux aswell
+
+## Finally, have fun with my program
+> I'm still learning so maybe you will found problems in the code, any help if really helpfull for me <3
--- adivina_el_numero.py
+++ adivina_el_numero.py
@@ -5,6 +5,7 @@ def adivina_el_numero():
     print("Bienvenido al juego 'Adivina el número'")
     numero_a_adivinar = random.randint(1, 100)
     juego_activo = True
+    rondas = 0
 
     while juego_activo:
         intento = input("Adivina el número (1-100) o escribe 'salir' para terminar: ")
@@ -15,13 +16,16 @@ def adivina_el_numero():
             try:
                 num = int(intento)
                 if num == numero_a_adivinar:
-                    print("¡Felicitaciones, adivinaste el número!")
+                    rondas = rondas + 1
+                    print(f"¡Felicitaciones, adivinaste el número! Lo has adivinado en {rondas} intentos.")
                     juego_activo = False
                 elif num < numero_a_adivinar:
                     print("El número a adivinar es más grande")
+                    rondas = rondas + 1
                 elif num > 100:
                     print("No puedes poner más de 100!")
                 else:
                     print("El número a adivinar es más pequeño")
+                    rondas = rondas + 1
             except ValueError:
                 print("Por favor, ingrese un número válido o escriba 'salir'")

544c76c731d060d4a74e283a3096d9cb4dafff12
mishhuu <adri.alvrzt@gmail.com>
2023-03-24 19:21:05 +0100

    test

--- acciones.py
+++ acciones.py
@@ -12,7 +12,7 @@ from actions.sumar import *
 from actions.adivina_el_numero import *
 while True:
     
-    accion = input("Que quieres hacer?: ").lower()
+    accion = input("Que quieres hacer?:").lower()
     if accion == "salir" or accion == "s":
         os.system("cls")
         break

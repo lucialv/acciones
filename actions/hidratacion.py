ac75dc84868a5a9fc0810f28aab10e0140931b1c
Mishu <adri.alvrzt@gmail.com>
2023-04-06 20:54:01 +0200

    Better hidratacion.py

--- actions/hidratacion.py
+++ actions/hidratacion.py
@@ -2,15 +2,24 @@ from plyer import notification
 import time
 
 def hidratacion():
-    tiempo = int(input("Cada cuanto tiempo quieres que te recuerde que tienes que hidratarte? (en segundos) "))
+    
     contador = 0
     while True:
-        notification.notify(
-            title="Tomar agua",
-            message="¡Recuerda tomar aguita! "+ "x"+ str(contador),
-            app_name="Hidratación",
-            timeout=10
-        )
-        time.sleep(tiempo)
-        contador += 1
-        
+        try:
+            tiempoo = float(input("Cada cuanto tiempo quieres que te recuerde que tienes que hidratarte? (en segundos y si es decimal en .) "))
+        except ValueError:
+            print("No puedes usar un , o escribir letras! ")
+            continue
+        finally:
+            break
+    tiempo = tiempoo
+    while True:
+            notification.notify(
+                title="Tomar agua",
+                message="¡Recuerda tomar aguita! "+ "x"+ str(contador),
+                app_name="Hidratación",
+                timeout=10
+            )
+            time.sleep(tiempo)
+            contador += 1   
+
